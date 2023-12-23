60281897a1c81ac4cab88022bdfa470a97e08378
Tr3yPaschal <trye#nerdskoolmedia.com>
2023-12-23 10:48:41 -0700

    Commition before adding dynamic divs and changing the way states work.

--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,93 @@
 import { useState } from 'react';
-import axios from 'axios';
 
 export default function Home() {
   const [message, setMessage] = useState('');
   const [response, setResponse] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null); // State to track error
+  const [error, setError] = useState(null);
 
   const sendMessage = async () => {
+    if (isLoading) return;
+
     setIsLoading(true);
-    setError(null); // Reset error state on new request
+    setError(null);
+    setResponse('');
+
     try {
-      const res = await axios.post('http://localhost:3000/', message, {
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-        timeout: 20 * 60000, // Timeout set to 20 minutes
+      const responseStream = await fetch('http://localhost:3000/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'text/plain' },
+        body: message
       });
-      setResponse(res.data);
+
+      if (!responseStream.ok) {
+        throw new Error(`HTTP error! status: ${responseStream.status}`);
+      }
+
+      const reader = responseStream.body.getReader();
+      const decoder = new TextDecoder();
+
+      reader.read().then(function processText({ done, value }) {
+        if (done) {
+          setIsLoading(false);
+          return;
+        }
+
+        setResponse(prev => prev + decoder.decode(value, { stream: true }));
+        return reader.read().then(processText);
+      });
     } catch (err) {
       console.error('Error sending message:', err);
-      // Display detailed error information
-      setError(err.response ? `${err.response.data} (Status: ${err.response.status})` : err.message);
-      setResponse(''); // Reset response on error
-    } finally {
+      setError(err.message || 'Failed to fetch');
       setIsLoading(false);
     }
   };
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-900">
-      <div className="mb-4 w-full max-w-md rounded-lg bg-white p-6 shadow-lg">
-        <p className="mb-4 text-m font-semibold text-gray-700">Response:</p>
-        {error && <p className="mb-4 text-red-500">Error: {error}</p>}
-        <p className="mb-4 p-4 rounded bg-gray-50 border-gray-100 text-gray-600">{response}</p>
-        <div className="flex gap-4">
+    <div className="flex flex-col h-screen">
+      {/* Header with Nav-Bar */}
+      <header className="bg-blue-800 text-white p-4">
+        <nav className="container mx-auto flex justify-between items-center">
+          <span className="text-xl font-bold">NS-OpenChatKit</span>
+          {/* Additional Nav items can be added here */}
+        </nav>
+      </header>
+
+      {/* Main Content */}
+      <main className="flex flex-col items-center justify-center flex-grow p-6 bg-gray-900" style={{ paddingTop: '25px', paddingBottom: '25px', height: 'calc(100% - 50px)' }}>
+        <div className="mb-4 w-full max-w-2xl rounded-lg bg-white p-6 shadow-lg" style={{ height: '100%' }}>
+          {error && <p className="mb-4 text-red-500">Error: {error}</p>}
+          <p className="mb-4 p-4 rounded bg-gray-50 border-gray-100 text-gray-600" style={{ overflowY: 'auto' }}>{response}</p>
+        </div>
+      </main>
+
+
+
+      {/* Sticky Footer */}
+      <footer className="bg-gray-800 text-white text-center p-4 sticky bottom-0 flex flex-col items-center">
+        <div className="relative w-full max-w-2xl">
           <input
             type="text"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            className="flex-1 rounded border border-gray-300 p-2 text-gray-700 focus:border-blue-500 focus:outline-none"
+            className="w-full pl-4 pr-20 py-3 rounded border border-gray-300 text-gray-700 focus:border-blue-500 focus:outline-none"
+            style={{ maxWidth: '800px' }}
           />
           <button
             onClick={sendMessage}
-            className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
+            disabled={isLoading}
+            className={`absolute right-1 top-1/2 transform -translate-y-1/2 rounded bg-blue-500 py-2 px-4 text-white focus:outline-none focus:ring focus:ring-blue-300 ${isLoading ? 'bg-gray-500' : 'hover:bg-blue-600'
+              }`}
+            style={{ top: '50%', transform: 'translateY(-50%)' }}
           >
-            Send Message
+            {isLoading ? 'Sending...' : 'Send'}
           </button>
         </div>
-        {isLoading && <div className="loader"></div>}
-      </div>
-    </main>
-  )
+        <p className="mt-4">
+          &copy; Copyright Nerdskool 2024 - Powered by: GPT neo
+        </p>
+      </footer>
+    </div>
+  );
+
 }
